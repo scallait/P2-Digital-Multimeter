df2c03dd900a7c85b2f34b89288fd1dc5063f24d
Sebastian Rosso <sebastianjrosso@gmail.com>
2022-11-13 16:00:44 -0800

    Created InputCapture Files

---
+++ InputCapture.c
@@ -0,0 +1,11 @@
+/*
+ * InputCapture.c
+ *
+ *  Created on: Nov 13, 2022
+ *      Author: Sebas
+ */
+#include "InputCapture.h"
+
+void InputCapture_init(){
+	//TO DO: Program Pins to Input Capture mode
+}
